5adf2337e47fe864b989f7a14f5b522a5c784fbd
Reycho <62747204+Reycho@users.noreply.github.com>
2022-06-29 14:25:52 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-def on_button_pressed_a():
-    global turn
-    turn += 20
-    pins.servo_write_pin(AnalogPin.P0, turn)
-    if turn > 180:
-        turn = 180
-input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_received_value(name, value):
+    led.toggle(0, 0)
+    pins.servo_write_pin(AnalogPin.P0, value)
+    serial.write_value("x", value)
+radio.on_received_value(on_received_value)
 
-def on_button_pressed_b():
-    global turn
-    turn += -20
-    pins.servo_write_pin(AnalogPin.P0, turn)
-    if turn < 0:
-        turn = 0
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-turn = 0
-turn = 0
+radio.set_group(42)
 
 def on_forever():
-    serial.write_value("x", turn)
+    pass
 basic.forever(on_forever)
+
